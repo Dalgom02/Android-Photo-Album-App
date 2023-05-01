9bf3cdfd2a5fe2cf66164686ad7b10f623364e4e
Jaqueline Carreon <63017636+Jaqueline-Carreon@users.noreply.github.com>
2023-05-01 14:38:55 -0400

    add/delete tags still needs work

--- app/src/main/java/com/example/androidphotos/DisplayView.java
+++ app/src/main/java/com/example/androidphotos/DisplayView.java
@@ -1,15 +1,25 @@
 package com.example.androidphotos;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import model.Album;
 import model.DataManager;
@@ -23,10 +33,18 @@ public class DisplayView extends AppCompatActivity {
     private Button previousButton;
     private Button nextButton;
 
+    private Button addTagButton;
+
+    private Button deleteTagButton;
+
     private int albumIndex;
     private int photoIndex;
     private Album currentAlbum;
     private List<Photo> photos;
+
+    private List<Album> albums;
+
+    private PhotoAdapter photoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +56,12 @@ public class DisplayView extends AppCompatActivity {
         peopleTagTextView = findViewById(R.id.people_tag_text_view);
         previousButton = findViewById(R.id.previous_button);
         nextButton = findViewById(R.id.next_button);
+        addTagButton = (Button) findViewById(R.id.addTagButton);
+        deleteTagButton = findViewById(R.id.deleteTagButton);
 
         albumIndex = getIntent().getIntExtra("albumIndex", -1);
         photoIndex = getIntent().getIntExtra("photoIndex", -1);
+        albums = DataManager.loadAlbums(this);
 
         if (albumIndex == -1 || photoIndex == -1) {
             finish();
@@ -69,11 +90,146 @@ public class DisplayView extends AppCompatActivity {
             }
             updatePhotoDisplay();
         });
+
+        addTagButton.setOnClickListener(v->{
+            Photo p = photos.get(photoIndex);
+            addTagDialog(p);
+            //startActivity(new Intent(DisplayView.this, PhotoView.class));
+        });
+
+
+        deleteTagButton.setOnClickListener(v->{
+            Photo p = photos.get(photoIndex);
+            deleteTagDialog(p);
+            //startActivity(new Intent(DisplayView.this, PhotoView.class));
+        });
+
+
+    }
+    private void deleteTagDialog(Photo p){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_delete_tag,null);
+        Spinner deleteTagSpin = view.findViewById(R.id.deleteSpin);
+
+        ArrayList<String> pTags = new ArrayList<>();
+        if(!(p.getPersonTags().isEmpty())){
+            for(String s: p.getPersonTags()){
+                String m = "Person " + s;
+                pTags.add(m);
+            }
+        }
+        if(!(p.getLocationTags().isEmpty())){
+            for(String sa : p.getLocationTags()){
+                String l = "Location " + sa;
+                pTags.add(l);
+            }
+        }
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,pTags);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        deleteTagSpin.setAdapter(adapter);
+
+
+        builder.setView(view)
+                .setPositiveButton("Delete Tag", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String delTag = deleteTagSpin.getItemAtPosition(deleteTagSpin.getSelectedItemPosition()).toString();
+                String[] split = delTag.split("\\s+");
+
+                if(split[0].equalsIgnoreCase("Location")){
+                   // currentAlbum.getPhotos().get(photoIndex).getLocationTags().remove(split[1]);
+                    p.getLocationTags().remove(split[1]);
+                }
+                if(split[0].equalsIgnoreCase("Person")) {
+                    p.getPersonTags().remove(split[1]);
+                }
+                DataManager.saveAlbums(DisplayView.this, albums);
+                updatePhotoDisplay();
+                Toast.makeText(DisplayView.this, "Tag deleted successfully", Toast.LENGTH_SHORT).show();
+            }
+        })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 
+    private void addTagDialog (Photo p){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_add_photo, null);
+        EditText personTagEditText = view.findViewById(R.id.person_tag_edit_text);
+        EditText locationTagEditText = view.findViewById(R.id.location_tag_edit_text);
+
+        builder.setView(view)
+                .setPositiveButton("Add Tag", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        if(personTagEditText.getText() == null && locationTagEditText.getText() == null) {
+                            Toast.makeText(DisplayView.this, "ERROR BOTH FIELDS CANNOT BE EMPTY", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+
+                            String personTags = personTagEditText.getText().toString().trim();
+                            String locationTags = locationTagEditText.getText().toString().trim();
+
+                            if (!personTags.isEmpty()) {
+                                String[] personTagArray = personTags.split(",");
+                                Set<String> uniquePersonTags = new HashSet<>();
+                                for (String tag : personTagArray) {
+                                    uniquePersonTags.add(tag.trim());
+                                }
+
+                                for (String uniqueTag : uniquePersonTags) {
+                                    p.addPersonTag(uniqueTag);
+                                }
+                            }
+
+                            if (!locationTags.isEmpty()) {
+                                String[] locationTagArray = locationTags.split(",");
+                                Set<String> uniqueLocationTags = new HashSet<>();
+                                for (String tag : locationTagArray) {
+                                    uniqueLocationTags.add(tag.trim());
+                                }
+
+                                for (String uniqueTag : uniqueLocationTags) {
+                                            p.addLocationTag(uniqueTag);
+                                }
+                            }
+
+
+                        }
+
+                        updatePhotoDisplay();
+                        DataManager.saveAlbums(DisplayView.this,albums);
+
+                        Toast.makeText(DisplayView.this, "Tag added successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
     private void updatePhotoDisplay() {
+        //photos = DataManager.loadAlbums(this).get(albumIndex).getPhotos();
         Photo currentPhoto = photos.get(photoIndex);
+
         Uri photoUri = Uri.parse(currentPhoto.getUri());
         photoDisplayImage.setImageURI(photoUri);
 
